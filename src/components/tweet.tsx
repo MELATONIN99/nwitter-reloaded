81cfa1accb2a03c19b62aaa330777780b39b4085
MELATONIN99 <sindogfood@naver.com>
2023-10-12 00:19:04 +0900

    style : 에딧모달 크기수정, 에딧창 취소아이콘 수정

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -18,6 +18,7 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
+    width: '50%',
   },
 };
 
@@ -75,7 +76,22 @@ width: 40px;
 border-radius: 20%;
 background: none;
 color: white;
-`
+`;
+
+
+const BackButton = styled.button`
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+border: 2px solid black;
+height: 30px;
+width: 40px;
+border-radius: 20%;
+background: white;
+color: black;
+margin-top: 10px;
+`;
 
 
 
@@ -130,7 +146,10 @@ export default function Tweet({username, photo, tweet, userId, id, createAt}:ITw
             tweet ={tweet}
             userId = {userId}
              id = {id}></EditTweetForm>
-            <button onClick={closeModal}>Close</button>
+            <BackButton onClick={closeModal}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M9.53 2.47a.75.75 0 010 1.06L4.81 8.25H15a6.75 6.75 0 010 13.5h-3a.75.75 0 010-1.5h3a5.25 5.25 0 100-10.5H4.81l4.72 4.72a.75.75 0 11-1.06 1.06l-6-6a.75.75 0 010-1.06l6-6a.75.75 0 011.06 0z" clipRule="evenodd" />
+</svg>
+</BackButton>
           </Modal>
         </div>
       );
