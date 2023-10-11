46d1eec88ac1276902037dd97fa809326b7a489d
MELATONIN99 <sindogfood@naver.com>
2023-10-11 23:51:34 +0900

    style : 삭제,수정 버튼 디자인 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -52,17 +52,30 @@ const Payload = styled.p`
 `;
 
 const DeleteButton = styled.button`
-background-color: tomato;
-color: white;
-font-weight: 600;
-border: 0;
-font-size: 12px;
-padding:5px 10px;
-text-transform: uppercase;
-border-radius: 5px;
 cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+border: 2px solid tomato;
+height: 30px;
+width: 40px;
+border-radius: 20%;
+background: none;
+color: tomato;
 `;
 
+const EditButton = styled.button`
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+border: 2px solid white;
+height: 30px;
+width: 40px;
+border-radius: 20%;
+background: none;
+color: white;
+`
 
 
 
@@ -84,7 +97,7 @@ export default function Tweet({username, photo, tweet, userId, id, createAt}:ITw
         }
     };
 
-
+  
     function EditModal() {
       const [modalIsOpen, setModalIsOpen] = useState(false);
     
@@ -99,7 +112,11 @@ export default function Tweet({username, photo, tweet, userId, id, createAt}:ITw
     
         return (
         <div>
-          <button onClick={openModal}>Edit</button>
+          <EditButton onClick={openModal}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+  <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+  <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+</svg>
+</EditButton>
           <Modal
             isOpen={modalIsOpen}
             onRequestClose={closeModal}
@@ -125,7 +142,10 @@ export default function Tweet({username, photo, tweet, userId, id, createAt}:ITw
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
         {user?.uid === userId ? <EditModal></EditModal> : null}
-        {user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
+        {user?.uid === userId ? <DeleteButton onClick={onDelete}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
+</svg>
+</DeleteButton> : null}
         </Column>
         <Column>
       {photo ? (
